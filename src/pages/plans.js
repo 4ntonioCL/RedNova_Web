db2a9b380298183d466c063b093ee99cde13be6b
unknown <1DAM-antoniocue491@IESMARTINEZM.ES>
2025-03-05 20:39:35 +0100

    Actualización de los planes

--- src/pages/plans.js
+++ src/pages/plans.js
@@ -1,18 +1,58 @@
-// Import's Component
-import NavBar from '../components/navbar'
-import Footer from '../components/footer'
+import './css/plans.css';
 
+// Import's Component
+import NavBar from '../components/navbar';
+import Footer from '../components/footer';
+
+import check from './check.svg';
 
 function Planes() {
+  const plans = [
+    {
+      name: 'Básico',
+      price: '$9.99/mes',
+      features: ['Acceso limitado', '1 usuario', 'Soporte básico'],
+    },
+    {
+      name: 'Estándar',
+      price: '$19.99/mes',
+      features: ['Acceso completo', 'Hasta 3 usuarios', 'Soporte prioritario'],
+    },
+    {
+      name: 'Premium',
+      price: '$29.99/mes',
+      features: ['Acceso VIP', 'Usuarios ilimitados', 'Soporte 24/7'],
+    },
+  ];
+
   return (
-    <body className="App">
+    <div className="App">
       <NavBar />
 
-      <h1>Planes</h1>
+      <h1 className='title'>Nuestros Planes</h1>
+      <div className='plans-container'>
+        {plans.map((plan, index) => (
+          <div key={index} className='plan-card'>
+            <h2>{plan.name}</h2>
+            <p className='price'>{plan.price}</p>
+            <ul>
+              {plan.features.map((feature, i) => (
+                <li key={i} className='text'>
+                  <img src={check} alt="check" className='check-icon' /> {feature}
+                </li>
+              ))}
+            </ul>
+            
+          </div>
+        ))}
+      </div>
 
       <Footer />
-    </body>
+    </div>
   );
 }
 
 export default Planes;
+
+
+// <button className='select-btn'>Seleccionar</button>
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,14 +1,27 @@
+import './css/services.css'
+
 // Import's Component
 import NavBar from '../components/navbar'
 import Footer from '../components/footer'
-
+import Servicio from '../components/servicio';
 
 function Services() {
   return (
     <body className="App">
       <NavBar />
 
-      <h1>Services</h1>
+      <div>
+        <h1 className='title'>Servicios</h1>
+
+        <div className='servicios'>
+          <Servicio nombre="Prueba" descripcion="Esto es una prueba de la descripción de un servicio" />
+          <Servicio nombre="Prueba" descripcion="Esto es una prueba de la descripción de un servicio" />
+          <Servicio nombre="Prueba" descripcion="Esto es una prueba de la descripción de un servicio" />
+          <Servicio nombre="Prueba" descripcion="Esto es una prueba de la descripción de un servicio" />
+          <Servicio nombre="Prueba" descripcion="Esto es una prueba de la descripción de un servicio" />
+          <Servicio nombre="Prueba" descripcion="Esto es una prueba de la descripción de un servicio" />
+        </div>
+      </div>
 
       <Footer />
     </body>
